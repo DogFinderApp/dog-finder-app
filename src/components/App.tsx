3b13025257c50d2c59fba8a4e93fa23f72c944ef
YanivWein24 <yanivwein22@gmail.com>
2023-11-11 15:36:51 +0200

    remove context completely

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { createStyleHook } from "../hooks/styleHooks";
 import { theme } from "../theme/theme";
 import { routesWithElements } from "../consts/routes";
-import { DogContextProvider } from "../context/DogContext";
 import { PageToolbar } from "./pageComponents/PageToolbar/PageToolbar";
 
 const useAppStyles = createStyleHook(() => {
@@ -21,23 +20,21 @@ export const App = () => {
   const styles = useAppStyles();
 
   return (
-    <DogContextProvider>
-      <ThemeProvider theme={theme}>
-        <Box sx={styles.root}>
-          <BrowserRouter>
-            <PageToolbar />
-            <Routes>
-              {routesWithElements.map((route) => (
-                <Route
-                  key={route.path}
-                  path={route.path}
-                  element={<route.element {...route.props} />}
-                />
-              ))}
-            </Routes>
-          </BrowserRouter>
-        </Box>
-      </ThemeProvider>
-    </DogContextProvider>
+    <ThemeProvider theme={theme}>
+      <Box sx={styles.root}>
+        <BrowserRouter>
+          <PageToolbar />
+          <Routes>
+            {routesWithElements.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={<route.element {...route.props} />}
+              />
+            ))}
+          </Routes>
+        </BrowserRouter>
+      </Box>
+    </ThemeProvider>
   );
 };
