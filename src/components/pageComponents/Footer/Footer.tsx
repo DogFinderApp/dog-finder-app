7778aa9ec80bb66e21caadb683885b878ec0cb42
YanivWein <yanivwein22@gmail.com>
2024-01-13 16:24:07 +0200

    larger footer on mobile

--- src/components/pageComponents/Footer/Footer.tsx
+++ src/components/pageComponents/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Box, Typography } from "@mui/material";
 import { createStyleHook } from "../../../hooks/styleHooks";
+import { useWindowSize } from "../../../hooks/useWindowSize";
 import { AppTexts } from "../../../consts/texts";
 import whatsappImg from "../../../assets/svg/whatsappGreen.svg";
 import facebookImg from "../../../assets/svg/facebook.svg";
@@ -12,7 +13,7 @@ const useFooterStyles = createStyleHook(() => ({
     margin: "1.5em 0 1rem",
   },
   typography: {
-    fontSize: 14,
+    fontSize: { sm: 14, xs: 16 },
     color: "white",
     textAlign: "center",
   },
@@ -35,8 +36,14 @@ const linkStyles = (isHover: boolean) => ({
   textDecoration: isHover ? "underline white" : "none",
 });
 
+const socialImageDimensions = (isMobile: boolean) =>
+  isMobile ? { width: 24, height: 24 } : { width: 22, height: 22 };
+const whatsappImageDimensions = (isMobile: boolean) =>
+  isMobile ? { width: 26, height: 26 } : { width: 24, height: 24 };
+
 export const Footer = () => {
   const styles = useFooterStyles();
+  const { isMobile } = useWindowSize();
   const [isHover, setIsHover] = useState<boolean>(false);
   const { joinUs, followUs } = AppTexts.footer;
 
@@ -63,7 +70,11 @@ export const Footer = () => {
         onMouseLeave={() => setIsHover(false)}
         style={linkStyles(isHover)}
       >
-        <img src={whatsappImg} alt="whatsapp" width={24} height={24} />
+        <img
+          src={whatsappImg}
+          alt="whatsapp"
+          style={whatsappImageDimensions(isMobile)}
+        />
         <Typography sx={styles.typography}>{joinUs}</Typography>
       </a>
       <Box sx={styles.socialsContainer}>
@@ -74,7 +85,11 @@ export const Footer = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={social.image} alt={social.alt} width={22} height={22} />
+            <img
+              src={social.image}
+              alt={social.alt}
+              style={socialImageDimensions(isMobile)}
+            />
           </a>
         ))}
         <Typography sx={styles.typography}>{followUs}</Typography>
