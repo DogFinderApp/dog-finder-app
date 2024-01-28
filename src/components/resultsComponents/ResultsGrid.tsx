6db343ff6a9f8a7a4f6a39afa7d7882f21729ccb
YanivWein <yanivwein22@gmail.com>
2024-01-28 22:09:56 +0200

    update bottom text in `ResultsGrid` to be display: fixed + add some animations to `ResultsPage` to improve the UX

--- src/components/resultsComponents/ResultsGrid.tsx
+++ src/components/resultsComponents/ResultsGrid.tsx
@@ -1,4 +1,5 @@
-import { Box, Grid, Typography, useTheme } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Box, Fade, Grid, Slide, Typography, useTheme } from "@mui/material";
 import { KeyedMutator } from "swr";
 import { createStyleHook } from "../../hooks/styleHooks";
 import { useWindowSize } from "../../hooks/useWindowSize";
@@ -10,12 +11,24 @@ import { NoDogs } from "./NoDogs";
 const useResultsStyles = createStyleHook(() => ({
   topTextStyle: {
     width: "100%",
-    fontSize: 20,
+    fontSize: { md: 20, xs: 18 },
     lineHeight: "25px",
     mb: 4,
     direction: "rtl",
     textWrap: "balance",
     textAlign: { sm: "right", xs: "center" },
+  },
+  floatingSection: {
+    position: "fixed",
+    bottom: 20,
+    left: 0,
+    right: 0,
+    width: { md: "max-content", xs: "80%" },
+    zIndex: 20,
+    backgroundColor: "rgba(25,25,25,0.85)",
+    margin: "0 auto",
+    padding: "0 1rem",
+    borderRadius: "4px",
   },
 }));
 
@@ -39,35 +52,49 @@ export const ResultsGrid = ({
   const styles = useResultsStyles();
   const { isMobile } = useWindowSize();
   const theme = useTheme();
+  const [slideIn, setSlideIn] = useState(false);
+
+  useEffect(() => {
+    // slide in animation delay
+    setTimeout(() => setSlideIn(true), 900);
+  }, []);
+
   const { topText1, topText2, dogNotFoundText } = AppTexts.resultsPage;
 
   return (
-    <Box width="100%">
-      {!allReportsPage && (
-        <Typography color={theme.palette.text.primary} sx={styles.topTextStyle}>
-          {topText1}
-          {isMobile ? " " : <br />}
-          {topText2}
-        </Typography>
-      )}
-      <Grid container spacing={4} dir="rtl">
-        {results?.map((dog) => (
-          <Grid item xs={12} sm={6} md={4} key={dog.dogId}>
-            <DogCard dog={dog} getUpdatedReports={getUpdatedReports} />
-          </Grid>
-        ))}
-      </Grid>
-      {!allReportsPage && (
-        <>
+    <Fade in timeout={500}>
+      <Box sx={{ width: "100%" }}>
+        {!allReportsPage && (
           <Typography
             color={theme.palette.text.primary}
-            sx={{ ...styles.topTextStyle, my: 4 }}
+            sx={styles.topTextStyle}
           >
-            {dogNotFoundText}
+            {topText1}
+            {isMobile ? " " : <br />}
+            {topText2}
           </Typography>
-          <NoDogs dogType={dogType} onlyNewReportButton />
-        </>
-      )}
-    </Box>
+        )}
+        <Grid container spacing={4} dir="rtl" mb={!allReportsPage ? 20 : 0}>
+          {results?.map((dog) => (
+            <Grid item xs={12} sm={6} md={4} key={dog.dogId}>
+              <DogCard dog={dog} getUpdatedReports={getUpdatedReports} />
+            </Grid>
+          ))}
+        </Grid>
+        {!allReportsPage && (
+          <Slide direction="up" timeout={500} in={slideIn}>
+            <Box sx={styles.floatingSection}>
+              <Typography
+                color={theme.palette.text.primary}
+                sx={{ ...styles.topTextStyle, my: 3, textAlign: "center" }}
+              >
+                {dogNotFoundText}
+              </Typography>
+              <NoDogs dogType={dogType} onlyNewReportButton />
+            </Box>
+          </Slide>
+        )}
+      </Box>
+    </Fade>
   );
 };
